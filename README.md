2dfd35eada9993735b152fda2362a411796dfa50
Amanda Shuman <amanda.n.shuman@gmail.com>
2021-08-10 20:10:33 -0400

    adding functions createBoard and flipCard

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Memory game involving HTML, CSS, and vanilla JavaScript 
 
-Artwork is courtsey of https://www.pinclipart.com/. The images have all been resized to 100 x 100 pixels for easier formatting later.
+Artwork is courtsey of https://www.pinclipart.com/. The images have all been resized to 100 x 100 pixels at https://resizepic.com/resize.php for easier formatting later.
 
 Memory Game, also known as the Concentration card game or Matching Game, is a simple card game where you need to match pairs by turn over 2 cards at a time. This game can be played solo or with teams. The person(s) with the most points, or pairs, at the end of the game wins. This game is designed with a solo player in mind. If playing with multiple players, please keep track of how many points each person gets.
 
--- app.js
+++ app.js
@@ -1,3 +1,124 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
+
+    //card options
+    const cardArray = [
+        {   //name needs to be the same name as the image for program to run
+            name: 'handsHeart',
+            img: 'images/handsHeart.png'
+        },
+        {
+            name: '2handsHeart',
+            img: 'images/2handsHeart.png'
+        },
+        {
+            name: 'angryPenguin',
+            img: 'images/angryPenguin.png'
+        },
+        {
+            name: 'angryPenguin',
+            img: 'images/angryPenguin.png'
+        },
+        {
+            name: 'balloons',
+            img: 'images/balloons.png'
+        },
+        {
+            name: 'balloons',
+            img: 'images/balloons.png'
+        },
+        {
+            name: 'heartInHand',
+            img: 'images/heartInHand.png'
+        },
+        {
+            name: 'heartInHand',
+            img: 'images/heartInHand.png'
+        },
+        {
+            name: 'helloKitty',
+            img: 'images/helloKitty.png'
+        },
+        {
+            name: 'helloKitty',
+            img: 'images/helloKitty.png'
+        },
+        {
+            name: 'key',
+            img: 'images/key.png'
+        },
+        {
+            name: 'key',
+            img: 'images/key.png'
+        },
+        {
+            name: 'openSign',
+            img: 'images/openSign.png'
+        },
+        {
+            name: 'openSign',
+            img: 'images/openSign.png'
+        },
+        {
+            name: 'pig',
+            img: 'images/pig.png'
+        },
+        {
+            name: 'pig',
+            img: 'images/pig.png'
+        },
+        {
+            name: 'present',
+            img: 'images/present.png'
+        },
+        {
+            name: 'present',
+            img: 'images/present.png'
+        },
+        {
+            name: 'raccoon',
+            img: 'images/raccoon.png'
+        },
+        {
+            name: 'raccoon',
+            img: 'images/raccoon.png'
+        }
+    ]
+
+    const grid = document.querySelector('.grid')
+    var cardsChosen = []
+    var cardsChosenId = []
+
+    //create your game board
+    function createBoard() {
+        for (let i = 0; i < cardArray.length; i++) {
+            var card = document.createElement('img')
+            card.setAttribute('src', 'images/allCardBack.jpg')
+            card.setAttribute('data-id', i)
+            //card.addEventListener('click', flipCard)
+            grid.appendChild(card)
+        }
+    }
+
+    //check for matches
+    function checkForMatch() {
+        var cards = document.querySelectorAll('img')
+        
+    }
+
+    //flip your card
+    function flipCard() {
+        var cardId = this.getAttribute('data-id')               //gets id from createBoard function
+        cardsChosen.push(cardArray[cardId].name)                //adds card name to array
+        cardsChosenId.push(cardId)                              //adds card id to array
+        this.setAttribute('src', cardArray[cardId].img)         //sets image to the image from the card id
+        if (cardsChosen === 2) {                                //want to invoke function check for match
+            setTimeout(checkForMatch, 500)                      //times out after 500ms when match found
+        }
+    }
+
+    createBoard()
+
+
+
+
 })
